9bffef07c31c0062ae37d7ac73914c2c775b0221
Tymoteusz Motylewski <t.motylewski@gmail.com>
2014-01-03 00:01:47 +0100

    [TASK] Cleanup irre_tutorial - remove backend module and sample data
    Change-Id: If84b17a793a80a064f667a2624da8abc30438abc

--- typo3/sysext/core/Tests/Functional/Fixtures/Extensions/irre_tutorial/ext_tables.php
+++ typo3/sysext/core/Tests/Functional/Fixtures/Extensions/irre_tutorial/ext_tables.php
@@ -2,20 +2,12 @@
 if (!defined ('TYPO3_MODE')) 	die ('Access denied.');
 
 	// ext_tables.php is split to each single part of application
-require(t3lib_extMgm::extPath($_EXTKEY)).'Configuration/ExtTables/ext_tables.general.php';
-require(t3lib_extMgm::extPath($_EXTKEY)).'Configuration/ExtTables/ext_tables.1nff.php';
-require(t3lib_extMgm::extPath($_EXTKEY)).'Configuration/ExtTables/ext_tables.mnasym.php';
-require(t3lib_extMgm::extPath($_EXTKEY)).'Configuration/ExtTables/ext_tables.mnmmasym.php';
-require(t3lib_extMgm::extPath($_EXTKEY)).'Configuration/ExtTables/ext_tables.mnsym.php';
-require(t3lib_extMgm::extPath($_EXTKEY)).'Configuration/ExtTables/ext_tables.mnattr.php';
-require(t3lib_extMgm::extPath($_EXTKEY)).'Configuration/ExtTables/ext_tables.1ncsv.php';
+require(\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath($_EXTKEY)).'Configuration/ExtTables/ext_tables.general.php';
+require(\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath($_EXTKEY)).'Configuration/ExtTables/ext_tables.1nff.php';
+require(\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath($_EXTKEY)).'Configuration/ExtTables/ext_tables.mnasym.php';
+require(\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath($_EXTKEY)).'Configuration/ExtTables/ext_tables.mnmmasym.php';
+require(\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath($_EXTKEY)).'Configuration/ExtTables/ext_tables.mnsym.php';
+require(\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath($_EXTKEY)).'Configuration/ExtTables/ext_tables.mnattr.php';
+require(\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath($_EXTKEY)).'Configuration/ExtTables/ext_tables.1ncsv.php';
 
-if (TYPO3_MODE === 'BE') {
-	t3lib_extMgm::addModule(
-		'web',
-		'txirretutorialM1',
-		'',
-		t3lib_extMgm::extPath($_EXTKEY) . 'mod1/'
-	);
-}
 ?>
